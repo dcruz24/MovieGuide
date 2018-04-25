f9f330e58a9bef37281423d2b8209ff6f02b5f74
David Cruz <dcruz24@illinois.edu>
2018-04-25 13:14:17 -0500

    MovieGuide1-- Project Updated UI Done?

--- app/src/main/java/edu/illinois/cs/cs125/movieguide/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/movieguide/MainActivity.java
@@ -8,10 +8,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
+    private Button button11;
+    private Button button12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button1 = findViewById(R.id. MovieButton1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -19,6 +31,35 @@ public class MainActivity extends AppCompatActivity {
              openActivity2();
             }
         });
+    button2 = findViewById(R.id.MovieButton2);
+    button2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity2();
+        }
+    });
+    button3 = findViewById(R.id.MovieButton3);
+    button3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity2();
+        }
+    });
+    button4 = findViewById(R.id.MovieButton4);
+    button4.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity2();
+        }
+    });
+    button5 = findViewById(R.id.MovieButton5);
+    button5.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity2();
+        }
+    });
+    button6
     }
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
